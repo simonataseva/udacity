8421bceb3b1397fd5156ec6d423c6e3c36bae01f
Simona Taseva Bedelovska <taseva.simonna@gmail.com>
2021-01-30 11:51:04 +0100

    Updates in the makeGrid function

--- project-pixel-art-maker-starter/designs.js
+++ project-pixel-art-maker-starter/designs.js
@@ -4,27 +4,26 @@ document.querySelector("#sizePicker").addEventListener("submit", makeGrid);
 // A function for drawing the table and paint the boxes by click
 function makeGrid() {
 
-    var height = document.getElementById("inputHeight").value;
-    var width = document.getElementById("inputWidth").value;
-    var table = document.getElementById("pixelCanvas");
+    let height = document.getElementById("inputHeight").value;
+    let width = document.getElementById("inputWidth").value;
+    let table = document.getElementById("pixelCanvas");
+
+    // Remove all table's child elements before creating the grid
+    table.innerHTML = '';
 
     // Loop into the height and width
-    for(var i = 0; i < height; i++) {
+    for(let i = 0; i < height; i++) {
         // Drawing the rows
-        var row = table.insertRow(0)
-        for(var j = 0; j < width; j++) {
+        let row = table.insertRow(0)
+        for(let j = 0; j < width; j++) {
             // Drawing the columns
             row.insertCell(0)
         }
     }
 
-    // Set click event listener on the color picker element
-    document.querySelector("#colorPicker").addEventListener("click", function() {
-        // Find all tds in the table and add click event listener on each td
-        document.querySelectorAll("#pixelCanvas td").forEach(td => td.addEventListener("click", function() {
-            // Set specific color on the clicked td
-            td.style.backgroundColor = document.getElementById("colorPicker").value;
-        }));
-    });
-
+    // Find all tds in the table and add click event listener on each td
+    document.querySelectorAll("#pixelCanvas td").forEach(td => td.addEventListener("click", function() {
+        // Set specific color on the clicked td
+        td.style.backgroundColor = document.getElementById("colorPicker").value;
+    }));
 }
